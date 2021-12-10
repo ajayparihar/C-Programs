4a6e6782c69141702ee7b39f99336700e7775277
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-10 21:44:56 +0530

    first commit

---
+++ calc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter First Number\n");
+        scanf("%d",&a);
+
+    printf("Enter Second Number\n   ");
+        scanf("%d",&b);
+        c=a+b;
+        printf("Sum of the numbers is %d",c);
+
+
+    return 0;
+}
