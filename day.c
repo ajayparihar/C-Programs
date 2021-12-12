24d57b76f0371318087953d52420a35be1bf6c34
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-12 21:44:04 +0530

    commit

---
+++ day.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int day;
+    printf("Enter A number (1-7)\n");
+    scanf("%d",&day);
+    
+    switch (day)
+    {
+    case 1:
+    printf("MONDAY");
+        break;
+    case 2:
+    printf("TUESDAY");
+        break;
+    case 3:
+    printf("WEDNESDAY");
+        break;
+    case 4:
+    printf("THURSDAY");
+        break;
+    case 5:
+    printf("FRIDAY");
+        break;
+    case 6:
+    printf("SATURDAY");
+        break;
+    case 7:
+    printf("SUNDAY");
+        break;
+    default:
+    printf("Invalid Input!!!");
+    }
+    return 0;
+}
