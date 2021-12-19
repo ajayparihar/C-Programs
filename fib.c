93ecf8467ee9d1f147c95d3768b3cf984523f7cb
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-19 21:07:57 +0530

    Fibonacci

---
+++ fib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib(int num)
+{
+    if (num == 0 || num == 1 || num == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return ((fib(num - 1)) + (fib(num - 2)));
+    }
+
+    return 0;
+}
+int main()
+{
+    int number;
+    printf("Enter a NUMBER\n");
+    scanf("%d", &number);
+    printf("%d", fib(number));
+    return 0;
+}
