7382a6438b55a827e06424cc3b8ce9b718da0ee2
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-23 18:54:09 +0530

    Size of data Types

---
+++ size_of_data_types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("Char is %d Bytes\n\n", sizeof(char));
+    printf("Short Int is %d Bytes\n\n", sizeof(short int));
+    printf("Long Int is %d Bytes\n\n", sizeof(long int));
+    printf("Float is %d Bytes\n\n", sizeof(float));
+    printf("Double is %d Bytes\n\n", sizeof(double));
+    printf("Long Double is %d Bytes\n\n", sizeof(long double));
+
+    return 0;
+}
