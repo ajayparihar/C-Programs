bf3a8444d1c73aeb329c739aee73e522656895a8
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-25 15:46:25 +0530

    Case, Digit, Special Character

---
+++ case_checker.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter a CHARACTER\n");
+    scanf("%c", &c);
+    if (c >= 'a' && c <= 'z')
+    {
+        printf("CHARACTER %c is in LOWERCASE\n", c);
+    }
+    else if (c >= 'A' && c <= 'Z')
+    {
+        printf("CHARACTER %c is in UPPERCASE\n", c);
+    }
+    else if (c >= '0' && c <= '9')
+    {
+        printf("CHARACTER %c is a DIGIT\n", c);
+    }
+    else
+    {
+        printf("CHARACTER %c is a SPECIAL CHARACTER\n", c);
+    }
+
+    return 0;
+}
