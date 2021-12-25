dea4f8e2469a23afed2d0082e689a8ea628663e7
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-25 14:58:59 +0530

    Greatest among three

--- gcd.c
+++ gcd.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-    int num1,num2,gcd;
+    int num1, num2, gcd;
     printf("Enter NUMBER 1\n");
     scanf("%d", &num1);
 
     printf("Enter NUMBER 2\n");
     scanf("%d", &num2);
 
-    for (int i = 1; i <= num1 && i<=num2; i++)
+    for (int i = 1; i <= num1 && i <= num2; i++)
     {
-        if(num1 % i==0 && num2 % i==0)
-        gcd=i;
+        if (num1 % i == 0 && num2 % i == 0)
+            gcd = i;
     }
-    printf("G.C.D of %d and %d is:- %d\n",num1,num2,gcd);
-    
+    printf("G.C.D of %d and %d is:- %d\n", num1, num2, gcd);
+
     return 0;
 }
---
+++ greatest_among_three.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, max1,max;
+
+    printf("Enter NUMBER 1\n");
+    scanf("%d", &num1);
+
+    printf("Enter NUMBER 2\n");
+    scanf("%d", &num2);
+
+    printf("Enter NUMBER 3\n");
+    scanf("%d", &num3);
+
+    max1=(num1>num2) ? num1:num2;
+    max=(max1>num3) ? max1:num3;
+    printf("GREATEST number between %d, %d and %d is:-  %d\n",num1,num2,num3,max);
+
+    return 0;
+}
