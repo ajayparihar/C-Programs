a132dd539e84ec251cde2c2ee47b1bbf23bc7bac
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-26 21:50:21 +0530

    Side of Triangle

---
+++ side_of_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int side1, side2, side3;
+
+    printf("Enter ANGLE 1 of TRIANGLE\n");
+    scanf("%d", &side1);
+
+    printf("Enter ANGLE 2 of TRIANGLE\n");
+    scanf("%d", &side2);
+
+    printf("Enter ANGLE 2 of TRIANGLE\n");
+    scanf("%d", &side3);
+
+        if (((side1 + side2) > side3) && ((side2 + side3 > side1)) && ((side3 + side1) > side2))
+    {
+        printf("The TRIANGLE with SIDE %d, %d and %d is VALID\n", side1, side2, side3);
+    }
+    else
+    {
+        printf("The TRIANGLE with SIDE %d, %d and %d is INVALID!!!\n", side1, side2, side3);
+    }
+
+    return 0;
+}
