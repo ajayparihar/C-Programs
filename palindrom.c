d942c0a466dc2ef11d199c74410a6f7dbad65ace
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-28 13:24:49 +0530

    Palindrome

---
+++ palindrom.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, rev = 0, rem,temp;
+    printf("Enter a NUMBER\n");
+    scanf("%d", &num);
+    temp=num;
+
+    while (num != 0)
+    {
+        rem = num % 10;
+        rev = (rev * 10) + rem;
+        num /= 10;
+    }
+
+    if (rev==temp)
+    {
+        printf("YES, the NUMBER %d is PALINDROME\n",temp);
+    }
+    else
+    {
+        printf("NO, the NUMBER %d is NOT PALINDROME\n",temp);
+    }
+    return 0;
+}
